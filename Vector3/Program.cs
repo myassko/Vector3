14723c39dc5a4e00d49743aec3ca842f2513ca2c
Помидорка <@DESKTOP-QD78RBG>
2022-10-27 23:09:30 +0300

    Добавьте файлы проекта.

---
+++ Vector3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Vector3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Vector a = new Vector(1, 4, 5);
+            Vector b = new Vector(1, 2, 4);
+
+            Console.WriteLine($"Вектор а-{a.ToString()}");
+            Console.WriteLine($"Вектор b-{b.ToString()}");
+
+            Console.WriteLine($"Длина вектора а={a.GetLength()}");
+            Console.WriteLine($"Длина вектора b={b.GetLength()}");
+
+            Vector v = a.GetAddition(b);
+            Console.WriteLine($"Сложение векторов a и б={v.ToString()}");
+
+            Vector z=a.GetSubtraction(b);
+            Console.WriteLine($"Вычитание векторов a и б={z.ToString()}");
+
+            Console.WriteLine($"Скалярное произведение a и b={a.Dot(b)}");
+
+    
+            Console.WriteLine($"Угол между а и b={a.GetAngle(b)} радиан");
+            Console.WriteLine($"Угол между а и b={(a.GetAngle(b)*180)/Math.PI} градусов");
+            Console.WriteLine($"Косинус угла между а и b={a.GetCos(b)}");
+
+        }
+    }
+}
---
+++ Vector3/Vector.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Vector3
+{
+    internal class Vector
+    {
+        private int x;
+        private int y;
+        private int z;
+
+        public Vector(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double GetLength()
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        public Vector GetAddition(Vector a)
+        {
+            return new Vector(x + a.x, y + a.y, z + a.z);
+        }
+        public Vector GetSubtraction(Vector a)
+        {
+            return new Vector(x - a.x, y - a.y, z - a.z);
+        }
+
+        public double Dot(Vector a)
+        {
+            return x*a.x+ y*a.y + z*a.z;
+        }
+
+
+        public double GetAngle(Vector a)
+        {
+            return Math.Acos(this.Dot(a)/(this.GetLength()*a.GetLength()));
+        }
+
+        public double GetCos(Vector a)
+        {
+            return Math.Cos(GetAngle(a));
+        }
+
+        public string ToString()
+        {
+            return $"({this.x},{this.y},{this.z})";
+        }
+    }
+}
